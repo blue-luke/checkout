f5345ff07a5fa292fdef2145c56f811eb7939c3d
Luke Blackburn <75164517+blue-luke@users.noreply.github.com>
2022-01-13 19:15:08 +0000

    4th user story complete, correctly formatting the price

--- lib/scanner.rb
+++ lib/scanner.rb
@@ -10,4 +10,17 @@ class Scanner
   def total
     @scanned_items.map { |item| item.price }.sum
   end
+  
+  def formatted_total
+    item_total = @scanned_items.map { |item| item.price }.sum.to_s
+    pounds, pence = item_total.split('.')[0], item_total.split('.')[1]
+    if pence.length == 1
+      pence.concat("0")
+    end
+    if pounds.length == 1
+      pounds.prepend("0")
+    end
+    "£#{pounds}.#{pence}"
+  end
+
 end
--- spec/scanner_spec.rb
+++ spec/scanner_spec.rb
@@ -6,4 +6,9 @@ describe Scanner do
     subject.scan(item)
     expect(subject.total).to eq 0.5
   end
+  it 'formats the total correctly' do
+    item = double("Item", :price => 0.5)
+    subject.scan(item)
+    expect(subject.formatted_total).to eq "£00.50"
+  end
 end
